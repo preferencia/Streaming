6b1fed6c4560f19568cb97ce26ee18148777bb24
preferencia <preferencia@naver.com>
2017-01-12 00:54:17 +0900

    Remove backup files

--- Common/SocketModule/Common/FSCryption.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "ClientBase64Url.h"
-
-void    SetKeySrcValue(char* pszSrc, int nSrcLen);
-int     MakeCryptionKey(char* pszIn, int nInputLen, char** pszOut);
-int     DecodeCryptionKey(char* pszIn, int nInputLen, char** pszOut);
